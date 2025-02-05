90d6edc015f354ac1abcfd55c3f56bac1fa24710
Skorplul <skorp1gaming@web.de>
2025-02-05 16:02:59 +0100

    fix

--- update-page.sh
+++ update-page.sh
@@ -1,7 +1,8 @@
-mv /var/www/html/.well-known /var/www/ -R
+mv /var/www/html/.well-known /var/www/ -r
 rm -rf /var/www/html/
 git clone https://github.com/Skorplul/homepage.git /var/www/html/
-mv update-page.sh /root/
-mv /var/www/.well-known /var/www/html/ -R
+rm update-page.sh
+mv /var/www/html/update-page.sh /root/
+mv /var/www/.well-known /var/www/html/ -r
 rm -rf /var/www/html/.git
 chmod +x update-page.sh 
