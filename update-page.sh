6f14e663aef9e976d49699c1e33b192615d574cc
Skorplul <skorp1gaming@web.de>
2025-02-05 15:59:53 +0100

    corrected

--- update-page.sh
+++ update-page.sh
@@ -1,5 +1,6 @@
-cp /var/www/html/.well-known /var/www/
+mv /var/www/html/.well-known /var/www/ -R
 rm -rf /var/www/html/
 git clone https://github.com/Skorplul/homepage.git /var/www/html/
-mv upadte-page.sh /root/
+mv update-page.sh /root/
+mv /var/www/.well-known /var/www/html/ -R
 rm -rf /var/www/html/.git
