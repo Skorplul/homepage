5bec8077d7290ab3c7eeb60286a04ff1cbf6a951
Skorplul <skorp1gaming@web.de>
2025-02-05 16:01:13 +0100

    added chmod

--- update-page.sh
+++ update-page.sh
@@ -4,3 +4,4 @@ git clone https://github.com/Skorplul/homepage.git /var/www/html/
 mv update-page.sh /root/
 mv /var/www/.well-known /var/www/html/ -R
 rm -rf /var/www/html/.git
+chmod +x update-page.sh 
