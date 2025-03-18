bc58bbca7a56c88a8a31df83bfb9e5cc936c9c7d
Skorplul <skorp1gaming@web.de>
2025-03-18 15:49:16 +0100

    update script

--- update-page.sh
+++ update-page.sh
@@ -1,6 +1,6 @@
 mv /var/www/html/.well-known /var/www/
 rm -rf /var/www/html/
-git clone https://github.com/Skorplul/homepage.git /var/www/html/
+git clone --depth 1 https://github.com/Skorplul/homepage.git /var/www/html/
 rm update-page.sh
 mv /var/www/html/update-page.sh /root/
 mv /var/www/.well-known /var/www/html/
